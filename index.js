ab0a15f2ab9ec444de8cbe8a5e9dcd62abbc3dad
Benedict Thompson <thomp334@coventry.ac.uk>
2020-10-27 18:18:04 +0000

    reformatted for consistent code style
    Applied code reformat for a consistent code style which helps prevent
    errors introduced by editing existing code, as well as making it
    easier to read.
    
    * use of braces in control statements
    * indentation
    * trailing commas
    * spacing between a control statements and their
      properties/conditions/etc.

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 import Koa from 'koa'
 import serve from 'koa-static'
 import views from 'koa-views'
@@ -15,15 +14,17 @@ const port = process.env.PORT || defaultPort
 async function getHandlebarData(ctx, next) {
 	console.log(`${ctx.method} ${ctx.path}`)
 	ctx.hbs = {
-		authorised: ctx.session.authorised
+		authorised: ctx.session.authorised,
 	}
-	for(const key in ctx.query) ctx.hbs[key] = ctx.query[key]
+	for (const key in ctx.query) {
+		ctx.hbs[key] = ctx.query[key]
+	}
 	await next()
 }
 
 app.use(serve('public'))
 app.use(session(app))
-app.use(views('views', { extension: 'handlebars' }, {map: { handlebars: 'handlebars' }}))
+app.use(views('views', {extension: 'handlebars'}, {map: {handlebars: 'handlebars'}}))
 
 app.use(getHandlebarData)
 

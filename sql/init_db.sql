235a9f6d1ecf08927a408f6dddce86a765fd1ebb
Benedict Thompson <thomp334@coventry.ac.uk>
2020-12-02 17:09:40 +0000

    created initialization script for database
    Starting work on a database repository layer to separate database access
    from other logic. It will be helpful to have a separate file from which
    the initialization commands can be loaded so they don't obscure the
    repository code.

---
+++ sql/init_db.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS users (
+    userID INTEGER PRIMARY KEY AUTOINCREMENT,
+    userName TEXT NOT NULL,
+    email TEXT NOT NULL,
+    cryptoPass TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS shows (
+    showID INTEGER PRIMARY KEY AUTOINCREMENT,
+    showName TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS show_dates (
+    showID INTEGER NOT NUll,
+    showDate DATE NOT NULL,
+    isMatinee BOOLEAN NOT NULL,
+    PRIMARY KEY (showID, showDate, isMatinee),
+    FOREIGN KEY (showID) REFERENCES shows(showID)
+);
